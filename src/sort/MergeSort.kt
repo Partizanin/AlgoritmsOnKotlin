98b799316f79bab1668f7d16d7c54389830cd859
Partizanin <topiciv1@yandex.ua>
2017-07-24 23:30:26 +0200

    add merge sort

---
+++ src/sort/MergeSort.kt
@@ -0,0 +1,57 @@
+package sort
+
+
+/**
+ * Created by Partizanin on 24.07.2017 18:54:36.
+ */
+class MergeSort {
+    private var numbers = intArrayOf()
+    private var tempArr = intArrayOf()
+    private var num = 0
+
+    fun sort(numbers: IntArray) {
+        this.numbers = numbers
+        num = numbers.size
+        tempArr = IntArray(num)
+        mergeSort(0, num - 1)
+    }
+
+    private fun mergeSort(low: Int, high: Int) {
+        if (low < high) {
+            val middle = low + (high - low) / 2
+
+            mergeSort(low, middle)
+
+            mergeSort(middle + 1, high)
+
+            merge(low, middle, high)
+        }
+    }
+
+    private fun merge(low: Int, middle: Int, high: Int) {
+        for (i in low..high) {
+            tempArr[i] = numbers[i]
+        }
+
+        var i = low
+        var j = middle + 1
+        var k = low
+
+        while (i <= middle && j <= high) {
+            if (tempArr[i] <= tempArr[j]) {
+                numbers[k] = tempArr[i]
+                i++
+            } else {
+                numbers[k] = tempArr[j]
+                j++
+            }
+            k++
+        }
+
+        while (i <= middle) {
+            numbers[k] = tempArr[i]
+            k++
+            i++
+        }
+    }
+}
---
+++ src/tests/sort/MergeSortTest.kt
@@ -0,0 +1,27 @@
+package sort
+
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.Test
+
+/**
+ * Created by Partizanin on 24.07.2017 20:09:33.
+ */
+class MergeSortTest {
+
+    @Test
+    fun sort() {
+        val ms = MergeSort()
+
+        var tempList = (1..100).toList()
+        tempList = tempList.shuffled()
+        val numbers = tempList.toIntArray()
+
+        ms.sort(numbers)
+        (0 until numbers.size - 1)
+                .filter { numbers[it] > numbers[it + 1] }
+                .forEach { fail() }
+        assertTrue(true)
+    }
+
+}
